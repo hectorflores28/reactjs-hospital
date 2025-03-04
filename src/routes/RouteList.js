541e5a89a4a7036cbec9196f4fcdc7809a369b35
Hector F <mielkren@gmail.com>
2025-03-03 21:15:20 -0600

    ReactJS Ajuste JSX

---
+++ src/routes/RouteList.js
@@ -0,0 +1,37 @@
+RouteList.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { AuthProvider } from '../context/AuthContext';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Dashboard from '../pages/Dashboard';
+
+function ProtectedRoute({ children }) {
+  const { user } = React.useContext(AuthContext);
+
+  return user ? children : <Navigate to="/login" replace />;
+}
+
+export default function RouteList() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          {/* Rutas Públicas */}
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+
+          {/* Rutas Protegidas */}
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
+}
